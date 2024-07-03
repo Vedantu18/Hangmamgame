f9ea2d53f36194b4bcc4b55b7554284e141e93a8
Vedant Ajay Shivsharan <vedantshivsharan14@gmail.com>
2024-07-03 14:42:44 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.dang.man;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(()-> new Dangman().setVisible(true));
+
+    }
+}
